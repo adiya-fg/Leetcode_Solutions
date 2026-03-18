ab880bf5d7d538dd5d4e6bc7ce43bf9d5d4b0ff8
Adiya <adiya.tb@gmail.com>
2026-03-18 13:03:44 +0900

    update

---
+++ 3070.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int ans=0;
+
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++) {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+grid[i-1][j-1];
+                if(dp[i][j]<=k) ans++;
+            }
+        }
+        return ans;
+    }
+};

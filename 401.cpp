2dbfadd37e5e57dd7bb374b0b4c7d8f62c9e7250
Adiya <adiya.tb@gmail.com>
2026-05-14 16:39:58 +0900

    update

--- 401.cpp
+++ 401.cpp
@@ -1,31 +1,22 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     vector<string> readBinaryWatch(int turnedOn) {
         vector<string> res;
-        
-        for(int i=0; i<12; i++) {
-            for(int j=0; j<60; j++) {
-                //count total bits
-                int count = 0;
-                int val = i;
-                while(val>0) {
-                    count+=(val%2==1);
-                    val/=2;
-                }
-                val = j;
-                while(val>0) {
-                    count+=(val%2==1);
-                    val/=2;
-                }
-                if(count==turnedOn) {
-                    string tempAns = to_string(i)+":";
-                    if(j<10) tempAns += "0";
-                    tempAns += to_string(j);
-                    res.push_back(tempAns);
+        for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                    string time = to_string(h) + ":";
+                    if (m < 10) time += "0";
+                    time += to_string(m);
+                    res.push_back(time);
                 }
             }
         }
-
         return res;
     }
 };
